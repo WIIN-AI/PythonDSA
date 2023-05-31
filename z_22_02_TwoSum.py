a3536cbcc4a0f4ebecf1eabf987f2fadc90f7b37
Vinaykumar Puppala <37238369+WIIN-AI@users.noreply.github.com>
2023-05-31 08:18:33 +0530

    Update z_22_02_TwoSum.py
    Added New Method

--- z_22_02_TwoSum.py
+++ z_22_02_TwoSum.py
@@ -26,3 +26,12 @@ for index in range(len(nums)):
         hast_tbl[target-nums[index]]=index
 
 print(result)
+
+
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    m = dict()
+    for i, v in enumerate(nums):
+        if target - v in m:
+            return [m[target - v], i]
+        m[v] = i
+    return []
